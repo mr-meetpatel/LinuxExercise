d8e04e6ad07d363df8bfea51917a881686350792
mr-meetpatel <pm2410.ldrp@gmail.com>
2022-05-27 15:49:22 +0530

    final commit

---
+++ ShellScript/4_lamp_setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+sudo apt install apache2
+apache2 -version
+sudo ufw app list
+sudo ufw allow ‘Apache’
+sudo ufw allow 'Apache'
+sudo ufw status
+sudo systemctl status apache2
+hostname -I
+sudo apt install PHP
+sudo apt install php
+php -v
+sudo apt install libapache2-mod-php
+sudo systemctl restart apache2
+sudo systemctl status apache2
+sudo apt install php-mysql
+
